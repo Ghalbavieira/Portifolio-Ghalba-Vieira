8d81a1e883c045fed12c75ff2a2bfc006beeda2e
Ghalba Vieira <gghalba@gmail.com>
2025-07-07 15:12:43 -0300

    chore: change layout and adjustment projetos

--- portifolio-ghalba-vieira/src/components/Footer/Footer.tsx
+++ portifolio-ghalba-vieira/src/components/Footer/Footer.tsx
@@ -4,13 +4,9 @@ import { SocialIcons } from "@/components/IconsSocial/SocialIcons";
 export function Footer() {
     return (
         <footer className="bg-black p-20 flex justify-around content-center">
-            <div>
+            <div className="text-center">
                 <h2 className="title text-slate-300">Ghalba Vieira</h2>
                 <h4 className="text-slate-300">Front End Developer</h4>
-                <SocialIcons/>
-            </div>
-            <div>
-                
             </div>
         </footer>
     );
